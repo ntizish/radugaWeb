c0d132f8a884df92a7ebfe34f655fe17165157b9
ntizish <dan.perednja@gmail.com>
2022-06-10 21:53:31 +0300

    second page

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,1 +1,15 @@
-//write here your js
+$(document).ready(function() {
+
+    let a = 0;
+    let b = 1000;
+    $(".runLineImg").clone().appendTo(".runLine");
+
+    $(".shopLink").click(function() {document.location.href="./shop.html"});
+
+
+
+
+
+
+
+});

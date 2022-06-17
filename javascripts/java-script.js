e13bac0d10bf4c7b84042f0460f4f88bb4185098
ntizish <dan.perednja@gmail.com>
2022-06-17 21:56:19 +0300

    UPD: picker 1 option working

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     let b = 1000;
     $(".runLineImg").clone().appendTo(".runLine");
 
+    $(".tanLink").click(function() {document.location.href="./tanPage.html"});
     $(".shopLink").click(function() {document.location.href="./shop.html"});
-    $(".tote, .envelope, .books, .cream, .creamToo, .eventsLink").hover(
+    $(".tote, .envelope, .books, .cream, .creamToo").hover(
         function () {
-            $(".tote, .envelope, .books, .cream, .creamToo").css("filter", "blur(0.3vw)");
-            $(".tote, .envelope, .books, .cream, .creamToo").css("transition", "filter 0.25s linear");
+            $(".tote, .envelope, .books, .cream, .creamToo").css("filter", "blur(0.3vw)").css("transition", "filter 0.25s linear");
             $(this).css("filter", "blur(0)");
             let item = $(".itemName"); //current item in heading (hidden atm)
             let price = $(".itemPrice"); //current price of the item in heading (hidden atm)
@@ -31,23 +31,99 @@ $(document).ready(function() {
             }
         },
         function () {
-            $(".tote, .envelope, .books, .cream, .creamToo").css("filter", "blur(0)");
-            $(".tote, .envelope, .books, .cream, .creamToo").css("transition", "filter 0.25s linear");
+            $(".tote, .envelope, .books, .cream, .creamToo").css("filter", "blur(0)").css("transition", "filter 0.25s linear");
             $(".itemPrice, .itemName").css("opacity", "0");
         }
     );
+    //option 1 works but not as good-looking
+    // redo in toggleClass
+    // mouseOn -> click inside?
+    let colorClicked = false;
+    let pickedTClass = null; // placeholder for future picked triangulars
 
     $(".picker img").hover(
         function () {
-            $(".picker img").css("transition", "transform 0.25s linear");
-            $(".picker img").css("transform", "scale(0.8, 0.8)");
-            $(this).css("transform", "scale(1.2, 1.2)");
+            let currentColor = $(".pickedColor"); //current color in heading (hidden atm)
+
+            let colorNameChanger = function () {
+                if (this.className === "purpleT pickedT") {
+                    currentColor.text(currentColor.text().replace(currentColor.text(), "пыльная лаванда"));
+                } else if (this.className === "redT pickedT"){
+                    currentColor.text(currentColor.text().replace(currentColor.text(), "сочинский персик"));
+                } else if (this.className === "greenT pickedT"){
+                    currentColor.text(currentColor.text().replace(currentColor.text(), "томный базилик"));
+                } else if (this.className === "yellowT pickedT"){
+                    currentColor.text(currentColor.text().replace(currentColor.text(), "горчичка"));
+                }
+            }
+
+            if (!colorClicked) {
+                $(".picker img").toggleClass("unpickedT");
+                pickedTClass = $(this);
+                $(this).toggleClass("unpickedT");
+                $(this).toggleClass("pickedT");
+                $(currentColor).toggleClass("activeColor");
+                colorNameChanger(); // loop must be placed here
+            } else if (colorClicked && !($(this).hasClass("pickedT"))) { // check if clicked T is already selected
+                $(this).click(function (){
+                    $(this).toggleClass("unpickedT");
+                    $(this).toggleClass("pickedT");
+                    pickedTClass.toggleClass("pickedT");
+                    pickedTClass.toggleClass("unpickedT");
+                    pickedTClass = $(this);
+
+                });
+            }
+
+
+            $(this).click(function (){
+                colorClicked = true;
+            });
         },
         function () {
-            $(".picker img").css("transition", "transform 0.25s linear");
-            $(".picker img").css("transform", "scale(1)");
-            $(this).css("transform", "scale(1)");
+            if (!colorClicked) {
+                $(this).toggleClass("pickedT");
+                $(".picker img").toggleClass("unpickedT");
+                $(this).toggleClass("unpickedT");
+                $(".pickedColor").toggleClass("activeColor");
+            }
         }
-    )
+    );
+
+    // option 2 not working properly TO:DO
+    // $(".picker img").hover(
+    //     function () {
+    //         $(".picker img").css("transition", "transform 0.25s linear");
+    //         $(".picker img").css("transition", "padding-right 0.25s linear");
+    //         $(this).css("padding-left", "9.2vw");
+    //         $(this).css("padding-right", "10vw");
+    //         $(this).css("transform", "scale(1.2, 1.2)");
+    //     },
+    //     function () {
+    //         $(".picker img").css("transition", "transform 0.25s linear");
+    //         $(".picker img").css("transition", "padding-right 0.25s linear");
+    //         $(this).css("padding-right", "9.6vw");
+    //         $(this).css("transform", "scale(1)");
+    //     }
+    // )
+
+    $(".picker img").click(
+        function () {
+            if (this.className === "purpleT") {
+                $(this).toggleClass("picked");
+
+            }
+        });
+
+
+
+    $(".sendButton").click(function () {
+        $(".workspaceOne").css("filter", "blur(1vw)").css("transition", "filter 0.25s linear").css("pointer-events", "none");
+        $(".scheduledDiv").css("opacity", "1").css("transition", "opacity 0.25s linear");
+        setTimeout(function () {
+            window.location.reload();
+        }, 4000);
+    });
+
 
 });
